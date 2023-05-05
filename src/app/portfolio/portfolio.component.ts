0478cf024e02b3162993f4f29d24d46ff216c822
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-05-05 18:48:06 +0200

    chnages

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class PortfolioComponent implements OnInit {
   @ViewChild('elPolloLoco') elPolloLoco: any;
   @ViewChild('join') join: any;
+  @ViewChild('crm') crm: any;
   hovered = false;
 
   zoomPolloLoco: string = /*html*/ `
@@ -15,6 +16,7 @@ export class PortfolioComponent implements OnInit {
       <div class="zoom-ani">
         <img class="img-position" src="./assets/img/pollo_loco_view.png" alt="#">
       </div>
+      <div class="layer"></div>
       <div class="button-aline">
         <button class="button-turquoise">Github</button>
         <button class="button-green">Live test</button>
@@ -39,22 +41,49 @@ export class PortfolioComponent implements OnInit {
       <div class="zoom-ani">
         <img class="img-position" src="./assets/img/join_view.png" alt="#">
       </div>
+      <div class="layer"></div>
       <div class="button-aline">
         <button class="button-turquoise">Github</button>
         <button class="button-green">Live test</button>
       </div>
       <div class="discribtion-box">
-          <div class="project-name"><h2>El Pollo Loco</h2></div>
-          <div class="description-text"><p>Jump, run and game based on object-oriented approach. 
-            Help Pepe to find coins and tabasco salsa to fight against the crazy hen.</p>
-          </div>
+          <div class="project-name"><h2>Join</h2></div>
+          <div class="description-text"><p>Task manager inspired by the Kanban System. 
+            Create and organize tasks using drag and drop functions, assign users and categories. 
+          </p>
+          </div> 
           <div class="code-introduction">JavaScript | HTML | CSS</div>
       </div>
     </div>
   `;
 
   zoomOutJoin: string = /*html*/ `
-    <div class="img-aline"><img src="./assets/img/pollo_loco.png" alt="#"></div>
+    <div class="img-aline"><img src="./assets/img/join.png" alt="#"></div>
+    <div class="drop-shadow"><img src="./assets/img/drop_shadow.png" alt="#"></div>
+  `;
+
+  zoomCrm: string = /*html*/ `
+    <div class="crm">
+      <div class="zoom-ani">
+        <img class="img-position" src="./assets/img/crm_view.png" alt="#">
+      </div>
+      <div class="layer"></div>
+      <div class="button-aline">
+        <button class="button-turquoise">Github</button>
+        <button class="button-green">Live test</button>
+      </div>
+      <div class="discribtion-box">
+          <div class="project-name"><h2>Simple CRM</h2></div>
+          <div class="description-text"><p>A very Simple Customer Relationship Management system working with CRUD functionality. 
+          </p>
+          </div> 
+          <div class="code-introduction">Angular | Firebase</div>
+      </div>
+    </div>
+  `;
+
+  zoomOutCrm: string = /*html*/ `
+    <div class="img-aline"><img src="./assets/img/crm.png" alt="#"></div>
     <div class="drop-shadow"><img src="./assets/img/drop_shadow.png" alt="#"></div>
   `;
 
@@ -69,13 +98,21 @@ export class PortfolioComponent implements OnInit {
   over(event: MouseEvent) {
     this.hovered = true;
     if (event.target === this.elPolloLoco.nativeElement) {
+      event.preventDefault();
       const zoomElPolloLoco = (event.target as HTMLElement);
       zoomElPolloLoco.innerHTML = this.zoomPolloLoco;
     }
     
     if (event.target === this.join.nativeElement) {
+      event.preventDefault();
       const zoomJoin = (event.target as HTMLElement);
       zoomJoin.innerHTML = this.zoomJoin;
+    }
+
+    if (event.target === this.crm.nativeElement) {
+      event.preventDefault();
+      const zoomCrm = (event.target as HTMLElement);
+      zoomCrm.innerHTML = this.zoomCrm;
     }
   }
   
@@ -89,6 +126,11 @@ export class PortfolioComponent implements OnInit {
     if (event.target === this.join.nativeElement) {
       const zoomJoin = (event.target as HTMLElement);
       zoomJoin.innerHTML = this.zoomOutJoin;
+    }
+
+    if (event.target === this.crm.nativeElement) {
+      const zoomCrm = (event.target as HTMLElement);
+      zoomCrm.innerHTML = this.zoomOutCrm;
     }
   }
 }
