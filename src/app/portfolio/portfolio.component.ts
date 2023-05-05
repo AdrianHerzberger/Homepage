f07992a53aa73c5670be35a11cdcfb200afe06f8
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-05-05 09:07:36 +0200

    chnages

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  over(event: MouseEvent) {
+    const zoomView = this.elementRef.nativeElement.querySelector('#zoomView');
+    this.renderer.addClass(zoomView, 'zoom-into-view');
+  }
+
+  out(event: MouseEvent) {
+    const zoomView = (event.target as HTMLElement);
+    zoomView.innerHTML = '';
+    zoomView.innerHTML += /*html*/`
+      
+     
+    `;
+  }
 
 }
